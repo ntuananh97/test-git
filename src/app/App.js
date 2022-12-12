689b5aef9765de02db12d807c34fff66805f8329
LAPTOP-UMM7HUUJ\user <ntuananh2897@gmail.com>
2022-12-12 23:11:08 +0700

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,80 @@
+import "../assets/styles/reset.css";
+import "../assets/styles/bootstrap-grid.min.css";
+import "../assets/styles/base.css";
+import "../assets/styles/global.scss";
+import movieImage from "../assets/images/one piece.jpg";
+import Footer from "./componentWeb/Footer";
+import Header from "./componentWeb/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className="main">
+        <div className="container">
+          <div className="hot-movie">
+            <h1 className="title-section">Phim hot</h1>
+
+            <div className="hot-movie-content">
+              {[...Array(5)].map((x, i) => (
+                <a href="/" className="movie-box" key={i}>
+                  <div className="movie-box__img">
+                    <img src={movieImage} alt="movie" className="img-objc" />
+                  </div>
+
+                  <div className="movie-sub">Tập 11 vietsub</div>
+                  <h2 className="movie-box-title">One Piece</h2>
+                  <i className="icon-play"></i>
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+            <div className="main-left col-12 col-lg-9">
+              <div className="cate-movie">
+                <div className="cate-movie__heading flex-a-c">
+                  <h2 className="tab-movie active">Phim bộ mới cập nhật</h2>
+                  <h2 className="tab-movie">
+                    Phim lẻ mới cập nhật
+                  </h2>
+                  <h2 className="tab-movie">
+                    Phim đã hoàn thành
+                  </h2>
+                </div>
+
+                <div className="cate-movie__main">
+                  <div className="row">
+                    {[...Array(5)].map((x, i) => (
+                      <div className="col-12 col-lg-3" key={i}>
+                        <a href="/" className="movie-box" >
+                          <div className="movie-box__img">
+                            <img
+                              src={movieImage}
+                              alt="movie"
+                              className="img-objc"
+                            />
+                          </div>
+
+                          <div className="movie-sub">Tập 11 vietsub</div>
+                          <h2 className="movie-box-title">One Piece</h2>
+                          <i className="icon-play"></i>
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="main-right col-12 col-lg-3">
+              <div className="incoming-movie">Phim sap chieu</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/componentWeb/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import logo from "../../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header-main flex-b-c">
+          <div className="left flex-a-c">
+            <a href="/" className="header-logo">
+              <img src={logo} alt="logo" />
+            </a>
+            <div className="header-search">
+              <form>
+                <div className="input-search">
+                  <input
+                    type="text"
+                    placeholder="V.d: tên phim, tên diễn viên..."
+                  />
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </form>
+
+              <p>Công cụ tìm kiếm phim.</p>
+            </div>
+          </div>
+
+          <div className="right">
+            <a href="/">
+              <i class="fa-solid fa-right-to-bracket"></i>
+              Login
+            </a>
+            <a href="/">
+              <i class="fa-solid fa-users"></i>
+              Sign up
+            </a>
+            <a href="/">
+              <i class="fa-regular fa-bookmark"></i>
+              Bookmark
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="header-menu">
+        <div className="container">
+          <ul className="header-list flex-a-c">
+            <li className="header-item">
+              <a href="/" className="header-link">Trang chu</a>
+            </li>
+            <li className="header-item">
+              <a href="/" className="header-link">The loai</a>
+
+              <ul className="header-list-child span-1">
+                {[...Array(15)].map((x, i) => (
+                  <li key={i} className="header-list-child-item">
+                    <a href="/">Cổ trang - Thần thoại</a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="header-item">
+              <a href="/" className="header-link">Quốc gia</a>
+
+              <ul className="header-list-child span-2">
+                {[...Array(11)].map((x, i) => (
+                  <li key={i} className="header-list-child-item">
+                    <a href="/">Việt Nam</a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="header-item">
+              <a href="/" className="header-link">Phim mới</a>
+
+              <ul className="header-list-child span-3">
+                {[...Array(7)].map((x, i) => (
+                  <li key={i} className="header-list-child-item">
+                    <a href="/">2020</a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="header-item" >
+              <a href="/" className="header-link">Phim bo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
